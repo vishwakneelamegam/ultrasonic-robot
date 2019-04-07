3e485da0791e75ffeb3fc6296dcc748e958ca4a1
vishwak <49344980+vishwakneelamegam@users.noreply.github.com>
2019-04-07 09:32:21 +0530

    Add files via upload

---
+++ ultrasonicrobot.ino
@@ -0,0 +1,73 @@
+//by vishwak(digital sailors)
+#define m1 2
+#define m2 3
+#define m3 4
+#define m4 5
+const int trigPin = 8;
+const int echoPin = 9; 
+long duration;
+int distance;
+String straight()
+{
+  digitalWrite(m1,HIGH);
+  digitalWrite(m2,LOW);
+  digitalWrite(m3,HIGH);
+  digitalWrite(m4,LOW);
+  return "STRAIGHT";
+}  
+String left()
+{
+  digitalWrite(m1,LOW);
+  digitalWrite(m2,HIGH);
+  digitalWrite(m3,HIGH);
+  digitalWrite(m4,LOW);
+  return "LEFT";
+} 
+String right()
+{
+  digitalWrite(m1,HIGH);
+  digitalWrite(m2,LOW);
+  digitalWrite(m3,LOW);
+  digitalWrite(m4,HIGH);
+  return "RIGHT";
+} 
+String back()
+{
+  digitalWrite(m1,LOW);
+  digitalWrite(m2,HIGH);
+  digitalWrite(m3,LOW);
+  digitalWrite(m4,HIGH);
+  return "BACK";
+}
+String finish()
+{
+  digitalWrite(m1,LOW);
+  digitalWrite(m2,LOW);
+  digitalWrite(m3,LOW);
+  digitalWrite(m4,LOW);
+  return "STOP";
+}
+void setup() {
+pinMode(trigPin, OUTPUT); 
+pinMode(echoPin, INPUT); 
+Serial.begin(9600); 
+}
+void loop() {
+digitalWrite(trigPin, LOW);
+delayMicroseconds(2);
+digitalWrite(trigPin, HIGH);
+delayMicroseconds(10);
+digitalWrite(trigPin, LOW);
+duration = pulseIn(echoPin, HIGH);
+// Calculating the distance
+distance= duration*0.034/2;
+Serial.print("Distance: ");
+Serial.println(distance);
+if(distance <= 10){
+  left();
+  }
+  else
+  {
+    straight();
+    }
+}
